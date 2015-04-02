777afab31c0e61be0ad089eb5033411b8d5deec1
Kamal Shree Soundirapandian <kamal.shree@sqills.com>
2015-04-02 20:00:33 +0200

    My seccond commit which display Doc details using combobox

--- src/myHospital.java
+++ src/myHospital.java
@@ -1,110 +1,85 @@
 import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.ItemSelectable;
+import java.awt.Label;
+import java.awt.TextArea;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.text.TextAction;
+
 import hospital.person.Doctor;
-import hospital.person.Person;
-import hospital.person.Receptionist;
-import hospital.rooms.Storage;
-import hospital.rooms.Testing;
 
 public class myHospital extends JFrame {
 
-	JPanel panel = new JPanel();
-	JButton docbutton = new JButton("Doctors");
-	JButton recpbutton = new JButton("Receptionist");
-	JButton clrbutton = new JButton("Clear");
-	JLabel list = new JLabel("List");
-	JTextArea textarea = new JTextArea(5, 10);
+	private void displaydocname() {
+
+		Doctor d1 = new Doctor(1000, "Jim", "Amsterdam", "Doc");
+		Doctor d2 = new Doctor(1001, "Jack", "Berlin", "Doc");
+		Doctor d3 = new Doctor(1002, "Karin", "Rotterdam", "Doc");
+		Doctor[] docarray = new Doctor[] { d1, d2, d3 };
+		final JTextArea textarea = new JTextArea();
+
+		JComboBox<Object> comboBox1 = new JComboBox<Object>(
+				new DefaultComboBoxModel(docarray));
+		comboBox1.setRenderer(new DefaultListCellRenderer() {
+			@Override
+			public Component getListCellRendererComponent(JList list,
+					Object value, int index, boolean isSelected,
+					boolean cellHasFocus) {
+				super.getListCellRendererComponent(list, value, index,
+						isSelected, cellHasFocus);
+				if (value instanceof Doctor) {
+					Doctor doc = (Doctor) value;
+					setText(doc.getName());
+				}
+				return this;
+			}
+		});
+
+		ActionListener getdetails = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox is = (JComboBox) e.getSource();
+				Doctor s = (Doctor) is.getSelectedItem();
+				textarea.setText(s.getAddress());
+			}
+
+		};
+
+		JPanel panel = new JPanel();
+		setSize(300, 400);
+		setResizable(true);
+		panel.add(new JLabel("List of Doctors:"));
+		panel.add(comboBox1);
+		panel.add(new JLabel("Doctors is From:"));
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(textarea);
+		comboBox1.addActionListener(getdetails);
+		add(panel);
+		setVisible(true);
+
+	}
 
 	public static void main(String[] args) {
 
-		new myHospital();
+		new myHospital().displaydocname();
+
 	}
-
-	public String listofdoctors() {
-		StringBuilder sb = new StringBuilder();
-		Doctor[] Doctorlist = new Doctor[3];
-		Doctorlist[0] = new Doctor(1000, "Jim", "Amsterdam", "Doc");
-		Doctorlist[1] = new Doctor(1001, "Jack", "Berlin", "Doc");
-		Doctorlist[2] = new Doctor(1002, "Karin", "Rotterdam", "Doc");
-
-		for (int i = 0; i < Doctorlist.length; i++) {
-			sb.append(Doctorlist[i].getName());
-			sb.append("\n");
-		}
-		return sb.toString();
-	}
-
-	public String Receptionlist() {
-		StringBuilder sb = new StringBuilder();
-		Receptionist[] replist = new Receptionist[2];
-		replist[0] = new Receptionist(2000, "Claire", "Munster", "Recp");
-		replist[1] = new Receptionist(2001, "Nancy", "Hengelo", "Recp");
-
-		for (int j = 0; j < replist.length; j++) {
-			sb.append(replist[j].getName());
-			sb.append("\n");
-		}
-		return sb.toString();
-	}
-
-	public myHospital() {
-
-		super("List of Doctors");
-		setSize(300, 400);
-		setResizable(true);
-
-		panel.add(docbutton);
-		panel.add(textarea);
-
-		panel.add(recpbutton);
-		panel.add(clrbutton);
-		panel.add(list);
-
-		docbutton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				textarea.setText(null);
-				textarea.setText(listofdoctors());
-			}
-
-		});
-		recpbutton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				textarea.setText(null);
-				textarea.setText(Receptionlist());
-			}
-
-		});
-		panel.add(textarea);
-
-		clrbutton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				textarea.setText(null);
-			}
-
-		});
-
-		add(panel);
-		setVisible(true);
-	}
-
 }
